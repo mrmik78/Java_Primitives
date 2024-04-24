5c347a9e1f53cb9b0daa33312947b8c581ef69b2
mrmik78 <m.reutsky@mail.ru>
2024-04-24 21:46:12 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int costTicket = 13676;
+        int oneBonusMiles = 20;
+        int totalBonusMiles;
+
+        System.out.println("Количество начисленных миль за купленный билет:");
+        //System.out.println("Количество начисленных миль за купленный билет: " + (costTicket / oneBonusMiles));
+        System.out.println(totalBonusMiles = costTicket / oneBonusMiles);
+    }
+}
